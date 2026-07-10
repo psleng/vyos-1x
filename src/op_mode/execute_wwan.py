bae1a4560d116b92de33b9f9737b5d54d4b9beb9
johnlfeeney <johnlfeeney@gmail.com>
2026-07-10 11:24:42 -0400

    command to stop wwan manager

---
+++ src/op_mode/execute_wwan.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# Copyright (C) 2024-2026 Perle Systems Limited
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 or later as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+import vyos.opmode
+from vyos.utils.process import call
+
+manager_unit = 'igos-wwan-manager.service'
+
+def _stop_manager_service():
+    """Stop the WWAN manager service."""
+    call(f'systemctl stop {manager_unit}')
+    return None
+
+
+def execute_stop_manager():
+    """Stop the WWAN manager service."""
+    _stop_manager_service()
+    return None
+
+
+if __name__ == '__main__':
+    try:
+        res = vyos.opmode.run(sys.modules[__name__])
+        if res:
+            print(res)
+    except (ValueError, vyos.opmode.Error) as e:
+        print(e)
+        sys.exit(1)
