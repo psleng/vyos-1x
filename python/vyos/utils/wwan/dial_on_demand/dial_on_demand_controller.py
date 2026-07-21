1e1027039101da25d2fb44d5e96c320e327120a0
Jessica Kong <jkong@perle.com>
2026-07-21 10:11:54 -0400

    Clean up controller

--- python/vyos/utils/wwan/dial_on_demand/dial_on_demand_controller.py
+++ python/vyos/utils/wwan/dial_on_demand/dial_on_demand_controller.py
@@ -2,30 +2,14 @@ import vyos.utils.wwan.dial_on_demand.idle_wwan_watcher as idle_wwan_watcher
 import vyos.utils.wwan.dial_on_demand.wwan_nft_reconnect2 as wwan_nft_reconnect
 import argparse
 import asyncio
-from vyos.utils.wwan.wwan_client import (  # noqa: E402
-    WWANClient,
-    WWANError,
-    WWANConfigError,
-    WWANConnectionError,
-)
 
 
-async def main(interface='wwan0', timeout=30, connect_timeout=30, loop=None):
+async def main(interface='wwan0', timeout=30, connect_timeout=30):
     """"""
     print("Start idle wwan watcher task")
-    client = WWANClient()
-    await client.open()
-    await client.add_interface(int(interface[4:]))
-    config = await client.set_configuration(int(interface[4:]), {
-            "connection_mode": "dial-on-demand",
-            "primary_sim_slot": 1,
-        })
-
-    idle_task = await idle_wwan_watcher.main(interface=interface, timeout=timeout, connect_timeout=connect_timeout, client=client)
+    idle_task = await idle_wwan_watcher.main(interface=interface, timeout=timeout, connect_timeout=connect_timeout)
     print("Start wwan reconnect task")
-    reconnect_task = await wwan_nft_reconnect.main(interface=interface,timeout=timeout, connect_timeout=connect_timeout, loop=loop, client=client)
-    print(await client.get_status(int(interface[4:])))
-    await client.close()
+    reconnect_task = await wwan_nft_reconnect.main(interface=interface,timeout=timeout, connect_timeout=connect_timeout)
 
 
 if __name__ == "__main__":
@@ -42,10 +26,4 @@ if __name__ == "__main__":
         timeout = args.interface.split(":")[1]
         connect_timeout = args.interface.split(":")[2]
 
-    loop = asyncio.get_event_loop()
-    try:
-        loop.run_until_complete(main(interface=interface,timeout=timeout,connect_timeout=connect_timeout,loop=loop))
-    finally:
-        loop.close()
-    #asyncio.run(main(interface=interface,timeout=timeout,connect_timeout=connect_timeout))
-    #asyncio.run(idle_wwan_watcher.main(interface=interface, timeout=timeout, connect_timeout=connect_timeout))
+    asyncio.run(main(interface=interface,timeout=timeout,connect_timeout=connect_timeout))
