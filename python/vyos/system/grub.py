0f808814d5ad87cb07b52a636936b86ffe388c36
Dennis Kong <dkong@perle.com>
2026-09-02 18:01:54 -0400

    add gzio module BEFORE linux modules so signature enforcement works for loading compressed linux kernel image

--- python/vyos/system/grub.py
+++ python/vyos/system/grub.py
@@ -90,7 +90,7 @@ def install(drive_path: str, boot_dir: str, efi_dir: str, id: str = 'VyOS', chro
             --force-extra-removable --boot-directory={boot_dir} \
             --efi-directory={efi_dir} --bootloader-id="{id}" \
             --uefi-secure-boot --no-nvram --pubkey=/gpgkeys/perle.pubkey.bin \
-            --modules="normal loadenv regexp configfile test echo linux probe search search_fs_uuid \
+            --modules="normal loadenv regexp configfile test echo gzio linux probe search search_fs_uuid \
                        part_gpt part_msdos ext2 fat serial terminal efi_gop pgp gcry_rsa gcry_sha256 gcry_sha512" '
 #            --uefi-secure-boot --no-nvram' 
     )
